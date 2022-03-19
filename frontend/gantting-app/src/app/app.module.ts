58e2fe60acbe617edcecded1c00da80649593281
Henrique Morais <rikeaju@hotmail.com>
2022-03-19 16:06:28 -0300

    Included Two Components

--- frontend/gantting-app/src/app/app.module.ts
+++ frontend/gantting-app/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { AssigneesComponent } from './assignees/assignees.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    AssignmentsComponent,
+      AssigneesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ frontend/gantting-app/src/app/assignees/assignees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignees',
+  templateUrl: './assignees.component.html',
+  styleUrls: ['./assignees.component.scss']
+})
+export class AssigneesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
