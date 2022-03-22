726588dcb679bd04dcf112da0e59893da19ed6bf
Henrique Morais <rikeaju@hotmail.com>
2022-03-21 21:22:58 -0300

    Nav Bar Created

--- frontend/gantting-app/src/app/app.module.ts
+++ frontend/gantting-app/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { AssigneesComponent } from './assignees/assignees.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
     AssignmentsComponent,
-    AssigneesComponent
+    AssigneesComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule
 ],
   providers: [],
   bootstrap: [AppComponent]
